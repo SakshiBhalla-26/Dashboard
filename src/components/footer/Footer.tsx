2a4c3db1bac1d934f6f32a22a2b78e09ff6db4e0
SakshiBhalla-26 <106884790+SakshiBhalla-26@users.noreply.github.com>
2024-12-02 22:43:37 +0530

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss'
+
+const Footer=()=> {
+  return (
+    <div className='footer'>
+      <span>R-Admin</span>
+      <span>© R-Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
